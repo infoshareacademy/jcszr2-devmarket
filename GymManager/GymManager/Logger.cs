2d546caa76d2cedc473d773a6a3fb5b33d2ebb1c
KrzysztofZiolkowski <Ziolek071@gmail.com>
2021-01-17 21:20:37 +0100

    Created new project with class&methods examples

---
+++ GymManager/GymManager/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManager
+{
+    static class Logger
+    {
+
+        public static void Log(string operation)
+        {
+
+        }
+
+    }
+}
