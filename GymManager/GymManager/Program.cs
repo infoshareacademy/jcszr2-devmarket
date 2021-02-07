3ea5464d7ccb34940d3118fcecba426e7565f8f0
Adam Dominik <adam.dominik89@gmail.com>
2021-02-07 19:21:27 +0100

    app is updated

--- GymManager/GymManager/Program.cs
+++ GymManager/GymManager/Program.cs
@@ -7,11 +7,7 @@ namespace GymManager
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            new App().RunApp();
-=======
             new App().Start();
->>>>>>> 00659e014f43e54c0e388badaf3da3c20a9bfce3
         }
     }
 }
