c0c2bda0175d5cb22ab76386d4c90172205125b9
KrzysztofZiolkowski <Ziolek071@gmail.com>
2021-03-08 23:31:25 +0100

    Added SignIn form fields

---
+++ GymManager/GymManagerWebApp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GymManagerWebApp.Models;
+
+namespace GymManagerWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            var user = new User();
+            return View(user);
+        }
+
+        [HttpPost] 
+        [ValidateAntiForgeryToken]
+        public IActionResult PostUser(User user)
+        {
+            //var isValid = ValidateUser();
+            return null;
+        }
+
+        //public bool ValidateUser(User user)
+        //{
+
+        //}
+    }
+}
---
+++ GymManager/GymManagerWebApp/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GymManagerWebApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        
+        [Required]
+        [DisplayName]
+        public string FirstName { get; protected set; }
+        
+        [Required]
+        public string SurName { get; protected set; }
+        
+        [Required]
+        [Phone]
+        public PhoneAttribute PhoneNr { get; protected set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; protected set; }
+        
+        [Required]
+        [PasswordPropertyText]
+        public PasswordPropertyTextAttribute Password { get; protected set; }
+
+        [Required]
+        [PasswordPropertyText]
+        public PasswordPropertyTextAttribute RepeatedPassword { get; protected set; }
+
+        [Required]
+        public string Gender { get; set; }
+       
+        public DateTime CreatedAt { get; protected set; }
+
+
+    }
+}
