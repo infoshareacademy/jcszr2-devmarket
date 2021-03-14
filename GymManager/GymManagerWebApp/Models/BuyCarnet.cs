5535e24f49c6b7a7e442bc9c21c82f3dff031189
Adam Dominik <adam.dominik89@gmail.com>
2021-03-14 19:51:28 +0100

    buy carnets subpage is finished

---
+++ GymManager/GymManagerWebApp/Models/BuyCarnet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManagerWebApp.Models
+{
+    public class BuyCarnet
+    {
+        private List<Carnet> carnetListTypes = new List<Carnet>()
+        {
+            new Carnet(1, "Karnet jednorazowy"), new Carnet(2, "Karnet 1 tygodniowy"), new Carnet(3, "Karnet 1 miesięczny")
+        };
+        public string TicketType { get; set; }
+
+        public List<Carnet> getCarnets()
+        {
+            return this.carnetListTypes;
+        }
+    }
+
+    public class Carnet
+    {
+        public int Id;
+        public string CarnetTypeName;
+
+        public Carnet(int id, string carnetTypeName)
+        {
+            this.Id = id;
+            this.CarnetTypeName = carnetTypeName;
+        }
+
+    }
+}
+
+
